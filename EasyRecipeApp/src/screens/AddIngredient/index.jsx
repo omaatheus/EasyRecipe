5938cd02ec2347c3b57418f81eb03459f14aa8d9
Isa <124785311+isacaramigo@users.noreply.github.com>
2024-08-15 17:08:28 -0300

    :sparkles: feat: Add Screens
    Add Screens: Add Recipe and Add Ingredients

---
+++ EasyRecipeApp/src/screens/AddIngredient/index.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+import { styles } from './styles'
+import React from 'react'
+
+export function AddIngredient() {
+  return (
+    <View style={styles.container}>
+      <Text>Add Ingredient Screen</Text>
+    </View>
+  )
+}
---
+++ EasyRecipeApp/src/screens/AddRecipe/index.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+import { styles } from './styles'
+import React from 'react'
+
+export function AddRecipe() {
+  return (
+    <View style={styles.container}>
+      <Text>Add Recipe Screen</Text>
+    </View>
+  )
+}
